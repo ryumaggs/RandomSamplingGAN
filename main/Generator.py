8e02185bd868a95b31d2e74212243d15b01cf90f
Ryan J. Yu <f006np8@lisplab4.thayer.dartmouth.edu>
2025-08-08 13:01:08 -0400

    moved files and changed names for production

--- main/Generator.py
+++ main/Generator.py
@@ -1,3 +1,4 @@
+#global imports
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
---
+++ main/experiments.py
@@ -0,0 +1,204 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import nn, optim
+from torch.nn import functional as F
+from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
+import copy
+from tqdm import tqdm
+import csv
+from scipy.special import softmax
+device = torch.device('cuda:0')
+import random
+import os
+
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='train_log.log', level=logging.INFO)
+
+from main.Dataset import Axios_ipsosdataset, HouseholdPulse_dataset, D4P_dataset, HouseholdPulse_synthetic, Ari_dataset
+from main.GAN import GAN, WGAN_GP, WGAN_GP_fict
+from main.Discriminator import DataDiscriminator
+from main.util import set_seed
+import itertools
+import pandas as pd
+#optimize over w directly, dont use any theta shenannigans. 
+from sklearn import preprocessing
+import pytorch_warmup as warmup
+from DataProcessing import *
+import pickle
+from itertools import combinations
+
+#NEW TRAINING VARIABLE SET UP
+# For learning
+#runs_consistency_SameData_SameSeen_diffNetwork,Seed:359556
+BATCH_SIZE = 4
+SUBSET_SIZE = 32
+GENERATOR_TRAINING_FACTOR = 1
+DISCRIMINATOR_TRAINING_FACTOR = 8
+GT_LIMIT = 25000
+BIAS_LIMIT = 2000
+LAMBDAGP = 0
+LAMBDAW = 1 #40
+LAMBDAD = 1
+GENERATOR_LEARNING_RATE = 1e-5 #5e-6
+DISCRIMINATOR_LEARNING_RATE = 1e-5 #1e-6
+BATCHS_IN_EPOCH = 1
+EPOCHS = 300 # the stream is infinite so one epoch will be defined as BATCHS_IN_EPOCH * BATCH_SIZE
+NUM_TRIALS = 1
+GEN_HISTORY_LENGTH = 1
+
+SAME_DATA_GT = False
+SAME_DATA_BIAS = False
+SAME_DATA_SEEN = False
+SAME_NETWORK_INIT = True
+#
+fixed_seed = [np.random.randint(0,1e6)]
+all_rngs = []
+print('SETTING SEED: ', fixed_seed)
+if type(fixed_seed) == list:
+    for _ in range(NUM_TRIALS):
+        t_rng = []
+        for seed in fixed_seed:
+            t_rng.append(set_seed(seed,
+                                    device,
+                                data_init=[SAME_DATA_GT, SAME_DATA_BIAS],
+                                data_gen =SAME_DATA_SEEN,
+                                network_init=SAME_NETWORK_INIT,))
+        all_rngs.append(t_rng)
+else:
+    for _ in range(NUM_TRIALS):
+        all_rngs.append(set_seed(fixed_seed,
+                                device,
+                            data_init=[SAME_DATA_GT, SAME_DATA_BIAS],
+                            data_gen =SAME_DATA_SEEN,
+                            network_init=SAME_NETWORK_INIT,))
+
+DEBUG_MODE = True
+
+if DEBUG_MODE:
+    BATCH_SIZE=2
+    SUBSET_SIZE=2
+    DISCRIMINATOR_TRAINING_FACTOR = 5
+    EPOCHS = 10
+    NUM_TRIALS = 1
+
+SAVE_EVERY = None
+
+GEN_DROPOUT = 0.2
+DISC_DROPOUT = 0.2
+
+#measures of consistency
+
+TEMPERATURE_START = 1
+TEMPERATURE_END = 0.3
+TEMPERATURE = 0.1
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# For dataset
+zero_prob = 0
+    
+GEN_LAYERS = [1024, 1024, 1024] #[256 for _ in range(5)]
+DISC_LAYERS = [1024, 1024, 1024]
+
+generator_types = ['dataGen'] #['dataGen','weightsGen','onesGen']
+
+regularizer_lambdas = [1]
+
+warmup_durations = [1]
+results = []
+'''
+Experiments to be done:
+1. Testing layer size
+2. Testing Learning Rate
+3. Testing Warmup_duration
+4. Testing Dropout
+5. testing uneven training
+'''
+
+def train(census_dataset_path,
+          survey_dataset_path,
+          save_path,
+          log_path,):
+
+    hparams = {
+        "glearningrate": GENERATOR_LEARNING_RATE,
+        "dlearningrate": DISCRIMINATOR_LEARNING_RATE,
+        "gen_layers": GEN_LAYERS,
+        "disc_layers": DISC_LAYERS,
+        "wudur": warmup_durations[0],
+        "generator_dropout": GEN_DROPOUT,
+        "discriminator_dropout": DISC_DROPOUT,
+        "gtrainingfactor": GENERATOR_TRAINING_FACTOR,
+        "dtrainingfactor": DISCRIMINATOR_TRAINING_FACTOR,
+        "subset_size": SUBSET_SIZE,
+        "batch_size": BATCH_SIZE,
+        "lambdagp": LAMBDAGP,
+        "lambdaw": LAMBDAW,
+        "lambdad": LAMBDAD,
+        "tau": TEMPERATURE,
+        "gen_history_length": GEN_HISTORY_LENGTH,
+    }
+    
+    if type(all_rngs) == list:
+        rngs = all_rngs[0]
+
+    d = HouseholdPulse_dataset(ground_truth_path=census_dataset_path,
+                    bias_path = survey_dataset_path,
+                    rngs=rngs,
+                    device=device,
+                    columns_to_keep = None,
+                    gt_limit = GT_LIMIT,
+                    bias_limit = BIAS_LIMIT, 
+                    )
+
+    gan = WGAN_GP(
+                rngs=rngs,
+                dataset=d,
+                generator_type='deepSet',
+                discriminator_type='deepSet',
+                gen_learning_rate=hparams["glearningrate"],
+                disc_learning_rate=hparams["dlearningrate"],
+                batch_size=hparams["batch_size"],
+                truth_sample_size=hparams["subset_size"],
+                gen_layers=hparams["gen_layers"],
+                disc_layers=hparams["disc_layers"],
+                bias_sample_size=hparams["subset_size"],
+                lambda_gp=hparams["lambdagp"],
+                lambda_weights=hparams["lambdaw"],
+                lambda_demo=hparams["lambdad"],
+                temperature=hparams["tau"],
+                warmup_length=hparams["wudur"],
+                lambda_regularizer=0,
+                generator_dropout=hparams["generator_dropout"],
+                discriminator_dropout=hparams["discriminator_dropout"],
+            )
+
+    writer = SummaryWriter(comment="")
+    
+    for key, item in hparams.items():
+        if isinstance(item, list):
+            hparams[key] = str(hparams[key])
+    
+    writer.add_hparams(hparams,{})
+
+    weights, bias_labels, prob_diffs,  test_probs, test_prob_diffs, generator_losses, discriminator_losses  = gan.train(BATCHS_IN_EPOCH,
+                                                                                                                EPOCHS,
+                                                                                                                TEMPERATURE_START,
+                                                                                                                TEMPERATURE_END,
+                                                                                                                hparams['gtrainingfactor'],
+                                                                                                                hparams['dtrainingfactor'],
+                                                                                                                SAVE_EVERY,
+                                                                                                                writer)
+    predicted_target = (weights @ bias_labels).item()
+    
+    #results.append((exp_var,cvar,predicted_target))
+
+    #with open('./results.pkl', 'wb') as file:
+    #    pickle.dump(results,file)
+    writer.close()
+
+#rename the runs folder into its appropriate name
+#if NUM_TRIALS > 1:
+#    os.rename("./runs", "./runs_week"+str(w))
