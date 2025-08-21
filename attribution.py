83c7793e833b5a4baa495f6e3ace8e84242cf26e
Ryan J. Yu <f006np8@lisplab4.thayer.dartmouth.edu>
2025-08-21 18:41:59 -0400

    added attribution function for IG computation

---
+++ attribution.py
@@ -0,0 +1,61 @@
+import torch
+import numpy as np
+
+def compute_equispaced_inputs_IG(baseline_input,actual_input,num_steps,unsqueeze=False):
+    '''
+    baseline_input - the mean , shape 1 x N
+    acutal_input - data set, shape M x N
+    num_steps - int - number of equispaced steps between baseline and actual
+
+    Finds num_steps equipspaced inputs between the baseline and actual input 
+    '''
+    all_inputs = []
+    # Compute difference from mu to each row in D
+    diff = actual_input - baseline_input  # 
+    # Generate interpolation coefficients between 0 and 1
+    alphas = torch.linspace(0, 1, steps=num_steps)  # 0 -> mu, 1 -> D
+
+    # Create list of interpolated matrices
+    for alpha in alphas:
+        # Linear interpolation for all rows
+        interp = baseline_input + alpha * diff
+        if unsqueeze:
+            all_inputs.append(interp.unsqueeze(0))
+        else:
+            all_inputs.append(interp)
+    return all_inputs
+
+def compute_IG(all_inputs,gan,network='generator'):
+    '''
+    all_inputs - list[torch.tensor] - returned by "compute equispaced_inputs_IG"
+    network - str - 'generator' or 'critic'
+    num_steps - int - number of steps between baseline and actual to compute gradient
+
+    1. iterates over each input in the list and computes the average gradient associated with the input
+    for the appropriate network type
+
+    '''
+    if network == 'generator':
+        all_grads = []
+        for ai in all_inputs:
+            X_for_grad = ai.detach().clone().requires_grad_(True)
+
+            # Forward pass through G then D
+            sds, selected_indices, weights = gan.generator(X_for_grad)
+            score = gan.discriminator(sds).sum()  # scalar score
+
+            # Compute gradient of score w.r.t. generator input
+            grad_input = torch.autograd.grad(
+                outputs=score,
+                inputs=X_for_grad,
+                retain_graph=False,
+                create_graph=False
+            )[0].detach()  # shape: (2500, 7)
+
+            all_grads.append(grad_input.cpu().numpy())
+        all_grads = np.concatenate(all_grads,axis=0)
+        return np.mean(all_grads,axis=0)
+    elif network == 'critic':
+        raise NotImplementedError
+    else:
+        raise NotImplementedError
